895d3d278a4f03b16574fa102cbc80b07fd989b9
t-john93 <tjohnson1293@gmail.com>
2018-08-31 15:29:37 -0700

    changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 //app.get has 2 args, name, and => function
 app.get("/", (req, res) => {
   // => has 2 args, req and response
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 }); //res calls .send with json 'hi there'
 
 const PORT = process.env.PORT || 5000; //dyn port var for heroku, or 5000 local
