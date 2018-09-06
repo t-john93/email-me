c0756c5de59706a1c0a4f32006e72c165a03258c
t-john93 <tjohnson1293@gmail.com>
2018-09-06 16:25:02 -0700

    made a few tweaks to keys.js and prod.js to possibly fix missing client_secret

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //Seperates development keys from production keys depending on enviornment
 
 //heroku sets NODE_ENV
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
