69530d03529a027991be343cd26f6ea392dc274c
t-john93 <tjohnson1293@gmail.com>
2018-12-24 05:58:32 -0800

    added component to render current user's survey history on dashboad

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
+import surveysReducer from "./surveysReducer";
 
 //method to combine reducers to a single object
 export default combineReducers({
     auth: authReducer,
-    form: reduxForm
+    form: reduxForm,
+    surveys: surveysReducer
 });
